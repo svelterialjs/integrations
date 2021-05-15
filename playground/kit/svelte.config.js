974f914cb704173b080277ab21975ec7e0b1cbe9
TheComputerM <mudit.harshit99@outlook.com>
2021-05-15 11:31:48 +0530

    updated packages

--- playground/kit/svelte.config.js
+++ playground/kit/svelte.config.js
@@ -1,9 +1,8 @@
-const svelterial = require('vite-plugin-svelte-svelterial');
+import svelterial from 'vite-plugin-svelte-svelterial';
 
 /** @type {import('@sveltejs/kit').Config} */
-module.exports = {
+export default {
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     vite: () => ({
       plugins: [
